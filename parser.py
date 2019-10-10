c15c18444826b14e3a0c17c4cd672459aab6194d
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-10-10 10:43:22 +0700

    the statement based equation is completed

--- parser.py
+++ parser.py
@@ -1,6 +1,27 @@
 #module tokenization
 import re
 
+def apply(string, sarg = {}, darg = {}):
+    return execute(to_postfix(tokenize(string)), sarg=sarg, darg=darg)
+
+def execute(statement, sarg = {}, darg = {}):
+    stack = []
+    for elem in statement :
+        if elem.isdigit() :
+            stack.append(int(elem))
+        elif elem in sarg :
+            operand = stack.pop()
+            stack.append(sarg[elem](operand))
+        elif elem in darg :
+            operandb = stack.pop()
+            operanda = stack.pop()
+            stack.append(darg[elem](operanda, operandb))
+    if len(stack) == 1 :
+        return stack.pop()
+    else:
+        return None
+
+
 def tokenize(strng):
     return filter(None, re.split("([+-/*!\(\)])", strng.replace(" ", "")))
 
@@ -12,6 +33,8 @@ def to_postfix(tokens):
             output.append(opt)
         elif opt == ")":
             while True:
+                if len(operst) == 0 :
+                    return []
                 temp = operst.pop()
                 if temp == "(" :
                     break;
@@ -19,6 +42,9 @@ def to_postfix(tokens):
                     output.append(temp)
         else:
             operst.append(opt)
+    for something in operst :
+        if something == "(" :
+            return []
     while True :
         output.append(operst.pop())
         if len(operst) == 0:
