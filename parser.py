b07f87e365e0f1b36650db6f4a884df2bd0a6706
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-10-10 09:11:57 +0700

    added the to postfix function

---
+++ parser.py
@@ -0,0 +1,43 @@
+#module tokenization
+
+#type of token:
+# Number   (N) :
+# Operator (O) : + - / * ^
+# Paren    (P) :
+def _token_type(thing):
+    return {'0':1,
+            '1':1,
+            '2':1,
+            '3':1,
+            '4':1,
+            '5':1,
+            '6':1,
+            '7':1,
+            '8':1,
+            '9':1,
+            '+':2,
+            '-':2,
+            '/':2,
+            '*':2,
+            '^':2,
+            '!':2,
+            '(':3,
+            ')':3,
+    }.get(thing, 4)
+
+
+def tokenize_string(strng):
+    return strng.split()
+
+def _infix_to_postfix(tokens):
+    res = []
+    waiting = 0
+    for i in tokens:
+        if i.isdigit():
+            res.append(i)
+            if waiting != 0:
+                res.append(waiting)
+                waiting = 0
+        else:
+            waiting = i
+    return res
