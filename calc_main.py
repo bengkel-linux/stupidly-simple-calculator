bf628bf35306e32903bba92aa2d79faacec337ba
alyanrndh <56344623+alyanrndh@users.noreply.github.com>
2019-10-10 22:59:50 +0700

    update the factorial

--- calc_main.py
+++ calc_main.py
@@ -44,6 +44,16 @@ while True:
         
         
     def factorial():
+        num = int(input("enter a number: "))
+        if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+        elif num == 0:
+        print("The factorial of 0 is 1")
+        else:
+        for i in range(1,num + 1):
+        factorial = factorial*i
+        print("The factorial of",num,"is:",factorial)
+        return
 
     if choice==1:
         additon()
