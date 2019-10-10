ba74e83a7864e2c8da4f91acb86420e04e974da0
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-10-10 09:54:09 +0700

    it convert the string to the reverse polish notation

--- parser.py
+++ parser.py
@@ -1,43 +1,26 @@
 #module tokenization
 import re
-#type of token:
-# Number   (N) :
-# Operator (O) : + - / * ^
-# Paren    (P) :
-def _token_type(thing):
-    return {'0':1,
-            '1':1,
-            '2':1,
-            '3':1,
-            '4':1,
-            '5':1,
-            '6':1,
-            '7':1,
-            '8':1,
-            '9':1,
-            '+':2,
-            '-':2,
-            '/':2,
-            '*':2,
-            '^':2,
-            '!':2,
-            '(':3,
-            ')':3,
-    }.get(thing, 4)
-
 
 def tokenize(strng):
-    return filter(None, re.split("([+-/*\(\)])", strng.replace(" ", "")))
+    return filter(None, re.split("([+-/*!\(\)])", strng.replace(" ", "")))
 
 def to_postfix(tokens):
-    res = []
-    waiting = 0
-    for i in tokens:
-        if i.isdigit():
-            res.append(i)
-            if waiting != 0:
-                res.append(waiting)
-                waiting = 0
+    output = []
+    operst = []
+    for opt in tokens:
+        if opt.isdigit():
+            output.append(opt)
+        elif opt == ")":
+            while True:
+                temp = operst.pop()
+                if temp == "(" :
+                    break;
+                else:
+                    output.append(temp)
         else:
-            waiting = i
-    return res
+            operst.append(opt)
+    while True :
+        output.append(operst.pop())
+        if len(operst) == 0:
+            break;
+    return output
